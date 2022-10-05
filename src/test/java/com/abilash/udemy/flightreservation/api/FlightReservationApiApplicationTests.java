fb77171d2a3a1bc26d15eb25e5a94a81e62003e1
Abilash Madhusudhanan <abilashmadhusudhanan@gmail.com>
2022-10-05 14:44:36 +0530

    Created spring boot project

---
+++ src/test/java/com/abilash/udemy/flightreservation/api/FlightReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abilash.udemy.flightreservation.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightReservationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
