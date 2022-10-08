917b068f4aa1793f1cd9c86f30b6621efce65ed4
Abilash Madhusudhanan <abilashmadhusudhanan@gmail.com>
2022-10-08 21:36:31 +0530

    Added entity classes

---
+++ src/main/java/com/abilash/udemy/flightreservation/api/entity/Flight.java
@@ -0,0 +1,55 @@
+package com.abilash.udemy.flightreservation.api.entity;
+
+import java.util.Date;
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Flight extends AbstractEntity {
+
+	private String flightNumber;
+	private String operatingAirlines;
+	private String depatureCity;
+	private String arrivalCity;
+	private Date dateOfDepature;
+	private Timestamp estimatedDepatureTime;
+	
+	public String getFlightNumber() {
+		return flightNumber;
+	}
+	public void setFlightNumber(String flightNumber) {
+		this.flightNumber = flightNumber;
+	}
+	public String getOperatingAirlines() {
+		return operatingAirlines;
+	}
+	public void setOperatingAirlines(String operatingAirlines) {
+		this.operatingAirlines = operatingAirlines;
+	}
+	public String getDepatureCity() {
+		return depatureCity;
+	}
+	public void setDepatureCity(String depatureCity) {
+		this.depatureCity = depatureCity;
+	}
+	public String getArrivalCity() {
+		return arrivalCity;
+	}
+	public void setArrivalCity(String arrivalCity) {
+		this.arrivalCity = arrivalCity;
+	}
+	public Date getDateOfDepature() {
+		return dateOfDepature;
+	}
+	public void setDateOfDepature(Date dateOfDepature) {
+		this.dateOfDepature = dateOfDepature;
+	}
+	public Timestamp getEstimatedDepatureTime() {
+		return estimatedDepatureTime;
+	}
+	public void setEstimatedDepatureTime(Timestamp estimatedDepatureTime) {
+		this.estimatedDepatureTime = estimatedDepatureTime;
+	}
+	
+}
